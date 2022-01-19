1e84eaed43c66e629b995daeaf38142def5e50bd
Khushboo Parasrampuria <khushboop09@gmail.com>
2022-01-19 15:36:46 +0530

    small fixes in leakybucket 2 implementation

--- leakyBucketAlgorithm2.py
+++ leakyBucketAlgorithm2.py
@@ -15,10 +15,10 @@ class Buffer:
         return str([str(self.buffer_size), str(self.buffer)])
 
 class LeakyBucket:
-    def __init__(self, capacity, forward_callback, drop_callback) -> None:
+    def __init__(self, capacity, output_rate, buffer_size, forward_callback, drop_callback) -> None:
         self.capacity = capacity
-        self.buffer = Buffer(5)
-        self.output_rate = 2
+        self.buffer = Buffer(buffer_size)
+        self.output_rate = output_rate
         self.tokens_in_bucket = str
         self.forward_callback = forward_callback
         self.drop_callback = drop_callback
@@ -34,7 +34,7 @@ class LeakyBucket:
                         self.buffer.buffer.append(request_packet[i])
                         count = len(self.buffer.buffer)
                     else:
-                        print("Data loss" + request_packet[i])
+                        self.drop_callback(request_packet[i])
         else:
             j=0
             for i in range(0, len(request_packet)+len(self.buffer.buffer)):
@@ -52,7 +52,7 @@ class LeakyBucket:
                             j += 1
                     else:
                         if j < len(request_packet):
-                            drop_callback(request_packet[j])
+                            self.drop_callback(request_packet[j])
                             j += 1
 
 # this function is called when a packet is forwarded
@@ -63,9 +63,9 @@ def forward_callback(request_packet):
 def drop_callback(no_of_request_packet):
     print("Request rate limited: " + str(no_of_request_packet))
 
-throttle = LeakyBucket(10, forward_callback, drop_callback)
+throttle = LeakyBucket(10, 2, 5, forward_callback, drop_callback)
 
 while True:
     time.sleep(1)
-    idata_to_send = input("Enter a string send by the server")
-    throttle.handle(idata_to_send)
+    request_packet = input("Enter a string send by the server: ")
+    throttle.handle(request_packet)
